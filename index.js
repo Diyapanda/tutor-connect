1e0209f0ec14feb097296ede6067d15992474afa
Diya Panda <137593678+Diyapanda@users.noreply.github.com>
2025-10-16 06:16:51 +0530

    Change default PORT from 5000 to 10000

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv")
 const app = express()
 const Routes = require("./routes/route.js")
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 10000
 
 dotenv.config();
 
@@ -21,6 +21,9 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
+
+app.options('*', cors())
+
 
 mongoose
     .connect(process.env.MONGO_URL, {
